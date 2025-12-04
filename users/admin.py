43210265635d9a6e33ea0a681f67c9c14c87326f
JperezRi03 <juan.perez@lgcns.com>
2025-12-04 10:31:16 -0500

    models register on admin, we can created books, loans and readers, next DRF

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Book, Reader, Loan
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(Reader)
+admin.site.register(Loan)
